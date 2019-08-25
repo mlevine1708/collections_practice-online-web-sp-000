64b554b80d5f6941497727546715771680efcf3c
Micah Levine <micah.levine@outlook.com>
2019-08-25 12:29:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(array)
+  array = [25, 7, 1]
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array = [25, 7, 14]
+  array.sort.reverse do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
